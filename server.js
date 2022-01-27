7fef4d2e0e16b235e9c973c4591976b45029cad7
jacob.swartzentruber <jsswart@gmail.com>
2022-01-27 10:35:17 -0600

    Added routes, user stories and styling
    Added routing on the server to pass all FCC tests. Added user stories to landing page and updated styling to make landing page cleaner with new HTML

--- server.js
+++ server.js
@@ -19,15 +19,16 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+//returns info about client
+app.get("/api/whoami", (req, res) => {
+  res.json({
+    ipaddress: req.ip,
+    language: req.get('accept-language'),
+    software: req.get("user-agent")
+  });
 });
 
-
-
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
